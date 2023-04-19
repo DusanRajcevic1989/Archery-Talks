9f20e29c622706d2cfa961e26e5ca82e94e915b7
DusanRajcevic1989 <dule.rajca@gmail.com>
2023-04-19 14:01:49 +0200

    add Components and first GET method

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: grid;
+  gap: 0.5rem;
+  margin-top: 1rem;
+  padding: 0.5rem;
+  position: relative;
+  width: 100%;
+  background-color: aliceblue;
+`;
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Archery Talks</title>
+      </Head>
+      <Main>{children}</Main>
+    </>
+  );
+}
---
+++ components/StyledButton.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: lightsalmon;
+  padding: 0.8rem;
+  border-radius: 0.6rem;
+  color: black;
+  font-weight: bold;
+  font-size: inherit;
+`;
